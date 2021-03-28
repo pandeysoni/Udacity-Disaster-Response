d621393594d5cff488458687e48c34c49e82ba9c
soni pandey <sonipandey.71@gmail.com>
2021-03-28 12:56:32 +0530

    first commit

---
+++ data/process_data.py
@@ -0,0 +1,103 @@
+
+# 1. import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+# 2. Merge datasets.
+# - Merge the messages and categories datasets using the common id
+# - Assign this combined dataset to `df`, which will be cleaned in the following steps
+def load_data():
+    """
+    Load Data function
+
+        df -> Loaded dasa as Pandas DataFrame
+    """
+    messages = pd.read_csv('./disaster_messages.csv')
+    categories = pd.read_csv('./disaster_categories.csv')
+
+    df = pd.merge(messages, categories, left_on='id', right_on='id')
+    return df 
+
+
+# ### 3. Split `categories` into separate category columns.
+# - Split the values in the `categories` column on the `;` character so that each value becomes a separate column. You'll find [this method](https://pandas.pydata.org/pandas-docs/version/0.23/generated/pandas.Series.str.split.html) very helpful! Make sure to set `expand=True`.
+# - Use the first row of categories dataframe to create column names for the categories data.
+# - Rename columns of `categories` with new column names.
+def clean_data(df):
+    """
+    Clean Data function
+    
+    Arguments:
+        df -> raw data Pandas DataFrame
+    Outputs:
+        df -> clean data Pandas DataFrame
+    """
+    categories = df.categories.str.split(pat=';',expand=True)
+    row = categories.iloc[0,:]
+    category_colnames = row.apply(lambda x:x[:-2])
+    categories.columns = category_colnames
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+
+    # ### 4. Convert category values to just numbers 0 or 1.
+    # - Iterate through the category columns in df to keep only the last character of each string (the 1 or 0). 
+    # For example, `related-0` becomes `0`, `related-1` becomes `1`. Convert the string to a numeric value.
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1:])
+        
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # ### 5. Replace `categories` column in `df` with new category columns.
+    # - Drop the categories column from the df dataframe since it is no longer needed.
+    # - Concatenate df and categories data frames.
+    df = df.drop('categories',axis=1)
+    df = pd.concat([df,categories],axis=1)
+
+    # ### 6. Remove duplicates.
+    # - Drop the duplicates.
+    df.drop_duplicates(inplace = True)
+    return df
+
+
+# ### 7. Save the clean dataset into an sqlite database.
+def save_data(df):
+    """
+    Save Data function
+    
+    Arguments:
+        df -> Clean data Pandas DataFrame
+    """
+    database_filename = 'Messages.db'
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('Messages', engine, index=False,  if_exists='replace')
+    pass  
+
+def main():
+    """
+    Main Data Processing function
+    
+    This function implement the ETL pipeline:
+        1) Load CSV File
+        2) Data cleaning and pre-processing
+        3) Data save to SQLite database
+    """
+    
+    df = load_data()
+
+    print('Cleaning data...')
+    df = clean_data(df)
+    
+    save_data(df)
+    
+    print('Cleaned data saved to database!')
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,179 @@
+"""
+TRAIN CLASSIFIER
+Disaster Resoponse Project
+Udacity - Data Science Nanodegree
+"""
+
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import re
+import pickle
+import nltk
+import os
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+
+import warnings
+
+warnings.simplefilter('ignore')
+
+def load_data():
+    """
+    Load Data Function
+    
+    Arguments:
+        database_filepath -> path to SQLite db
+    Output:
+        X -> feature DataFrame
+        Y -> label DataFrame
+        category_names -> used for data visualization (app)
+    """
+    database_filepath = 'Messages.db'
+    cwd = os.getcwd()
+    dbwd = cwd.replace('/models','/data/').replace('\\models','\\data\\')
+    engine = create_engine('sqlite:///'+dbwd+database_filepath)
+    print(engine.table_names())
+    df = pd.read_sql_table('Messages',engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    return X, Y, category_names
+
+
+def tokenize(text):
+    """
+    Tokenize function
+    
+    Arguments:
+        text -> list of text messages (english)
+    Output:
+        clean_tokens -> tokenized text, clean for ML modeling
+    """
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # Tokenize words
+    tokens = word_tokenize(text)
+    
+    # Stem word tokens and remove stop words
+    stemmer = PorterStemmer()
+    stop_words = stopwords.words("english")
+    
+    stemmed = [stemmer.stem(word) for word in tokens if word not in stop_words]
+    
+    return stemmed
+
+
+def build_model():
+    """
+    Build Model function
+    
+    This function output is a Scikit ML Pipeline that process text messages
+    according to NLP best-practice and apply a classifier.
+
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Evaluate Model function
+    
+    This function applies ML pipeline to a test set and prints out
+    model performance (accuracy and f1score)
+    
+    Arguments:
+        model -> Scikit ML Pipeline
+        X_test -> test features
+        Y_test -> test labels
+        category_names -> label names (multi-output)
+    """
+    Y_test_pred = model.predict(X_test)
+    Y_pred_pd = pd.DataFrame(Y_test_pred, columns = category_names)
+    for column in category_names:
+        print('------------------------------------------------------\n')
+        print('FEATURE: {}\n'.format(column))
+        print(classification_report(Y_test[column],Y_pred_pd[column]))
+
+    # Print the whole classification report.
+    # Extremely long output
+    # Work In Progress: Save Output as Text file!
+    
+    #Y_pred = pd.DataFrame(Y_pred, columns = Y_test.columns)
+    
+    #for column in Y_test.columns:
+    #    print('Model Performance with Category: {}'.format(column))
+    #    print(classification_report(Y_test[column],Y_pred[column]))
+    pass
+
+
+def save_model(model):
+    """
+    Save Model function
+    
+    This function saves trained model as Pickle file, to be loaded later.
+    
+    Arguments:
+        model -> GridSearchCV or Scikit Pipelin object
+    
+    """
+
+    filename = 'classifier.pkl'
+    pickle.dump(model, open(filename, 'wb'))
+    pass
+
+
+def main():
+    """
+    Train Classifier Main function
+    
+    This function applies the Machine Learning Pipeline:
+        1) Extract data from SQLite db
+        2) Train ML model on training set
+        3) Estimate model performance on test set
+        4) Save trained model as Pickle
+    
+    """
+    X, Y, category_names = load_data()
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, )
+    
+    print('Building model...')
+    model = build_model()
+    
+    print('Training model...')
+    model.fit(X_train, Y_train)
+    
+    print('Evaluating model...')
+    evaluate_model(model, X_test, Y_test, category_names)
+
+    print('Saving model...\n ')
+    save_model(model)
+
+    print('Trained model saved!')
+
+
+
+if __name__ == '__main__':
+    main()
